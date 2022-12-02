cc3c55525cfa479b3f416c349af33363d84f89e0
Paolo Sciarra <itbhps@gmail.com>
2022-12-02 23:03:15 +0100

    [adc2022] day02 part 2 solved

--- src/main/java/it/twinsbrain/adc2022/day02/Day02Solution.java
+++ src/main/java/it/twinsbrain/adc2022/day02/Day02Solution.java
@@ -10,8 +10,9 @@ public class Day02Solution {
 
     public static void main(String[] args) throws URISyntaxException {
         var input = read(resource("/day02/input02.txt"));
-        System.out.printf("Score: %d", score(input));
+        System.out.printf("Score part 1: %d", score(input));
         System.out.println();
+        System.out.printf("Score part 1: %d", scoreExpected(input));
     }
 
     private static final int FIRST_PLAYER = 0;
@@ -23,6 +24,32 @@ public class Day02Solution {
                 .map(Day02Solution::roundScore)
                 .mapToInt(Integer::intValue)
                 .sum();
+    }
+
+    public static int scoreExpected(List<String> input) {
+        return input.stream()
+                .map(l -> l.split(" "))
+                .map(Day02Solution::fromExpectedResultToMove)
+                .map(Day02Solution::roundScore)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private static String[] fromExpectedResultToMove(String[] input) {
+        var firstPlayerMove = adaptFirstPlayerMove(input[FIRST_PLAYER]);
+        var expectedResult = input[SECOND_PLAYER];
+        var result = switch (expectedResult) {
+            case "X" -> Result.Lost;
+            case "Y" -> Result.Draw;
+            default -> Result.Won;
+        };
+        var secondPlayerMove = switch (firstPlayerMove.opponentMoveGiven(result)) {
+            case Rock r -> "X";
+            case Paper p -> "Y";
+            case Scissor s -> "Z";
+        };
+
+        return new String[]{input[FIRST_PLAYER], secondPlayerMove};
     }
 
     private static int roundScore(String[] moves) {
@@ -56,6 +83,8 @@ public class Day02Solution {
 sealed abstract class Move permits Rock, Paper, Scissor {
     abstract Result winOver(Move other);
 
+    abstract Move opponentMoveGiven(Result opponentResult);
+
     abstract int points();
 }
 
@@ -67,6 +96,15 @@ final class Rock extends Move {
             case Rock r -> Result.Draw;
             case Paper p -> Result.Lost;
             case Scissor s -> Result.Won;
+        };
+    }
+
+    @Override
+    Move opponentMoveGiven(Result opponentResult) {
+        return switch (opponentResult) {
+            case Won -> new Paper();
+            case Draw -> new Rock();
+            case Lost -> new Scissor();
         };
     }
 
@@ -88,6 +126,15 @@ final class Paper extends Move {
     }
 
     @Override
+    Move opponentMoveGiven(Result opponentResult) {
+        return switch (opponentResult) {
+            case Won -> new Scissor();
+            case Draw -> new Paper();
+            case Lost -> new Rock();
+        };
+    }
+
+    @Override
     int points() {
         return 2;
     }
@@ -101,6 +148,15 @@ final class Scissor extends Move {
             case Rock r -> Result.Lost;
             case Paper p -> Result.Won;
             case Scissor s -> Result.Draw;
+        };
+    }
+
+    @Override
+    Move opponentMoveGiven(Result opponentResult) {
+        return switch (opponentResult) {
+            case Won -> new Rock();
+            case Draw -> new Scissor();
+            case Lost -> new Paper();
         };
     }
 

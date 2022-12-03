e263c39647369e7614bb69c2ac7e86ad1c7a7c38
Paolo Sciarra <itbhps@gmail.com>
2022-12-03 16:46:48 +0100

    [adc2022] mine refactoring

--- src/main/java/it/twinsbrain/adc2022/day01/Day01Solution.java
+++ src/main/java/it/twinsbrain/adc2022/day01/Day01Solution.java
@@ -1,7 +1,10 @@
 package it.twinsbrain.adc2022.day01;
 
 import java.net.URISyntaxException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static it.twinsbrain.adc2022.FilesModule.read;
 import static it.twinsbrain.adc2022.FilesModule.resource;
@@ -16,14 +19,22 @@ public class Day01Solution {
     }
 
     public static int maxCaloriesByThreeElves(List<String> input) {
-        String[] caloriesByElf = String.join("\n", input).split("\n\n");
-        return Arrays.stream(caloriesByElf)
-                .mapToInt(s -> Arrays.stream(s.split("\n")).mapToInt(Integer::valueOf).sum())
-                .boxed()
+        return caloriesByElf(input)
                 .sorted(Collections.reverseOrder())
                 .limit(3)
                 .mapToInt(Integer::intValue)
                 .sum();
+    }
+
+    private static Stream<Integer> caloriesByElf(List<String> input) {
+        String[] caloriesByElf = String.join("\n", input).split("\n\n");
+        return Arrays.stream(caloriesByElf)
+                .mapToInt(Day01Solution::singleElfCalories)
+                .boxed();
+    }
+
+    private static int singleElfCalories(String s) {
+        return Arrays.stream(s.split("\n")).mapToInt(Integer::valueOf).sum();
     }
 
 

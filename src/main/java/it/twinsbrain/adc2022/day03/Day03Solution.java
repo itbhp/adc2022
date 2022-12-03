c7ae141012fd9ae00ef22ca147be36904082ecea
Paolo Sciarra <itbhps@gmail.com>
2022-12-03 13:10:59 +0100

    [adc2022] day03 refactoring

--- src/main/java/it/twinsbrain/adc2022/day03/Day03Solution.java
+++ src/main/java/it/twinsbrain/adc2022/day03/Day03Solution.java
@@ -1,12 +1,17 @@
 package it.twinsbrain.adc2022.day03;
 
 import java.net.URISyntaxException;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static it.twinsbrain.adc2022.FilesModule.read;
 import static it.twinsbrain.adc2022.FilesModule.resource;
+import static java.util.stream.Collectors.toMap;
 import static java.util.stream.Collectors.toSet;
 
 public class Day03Solution {
@@ -18,60 +23,17 @@ public class Day03Solution {
         System.out.printf("Priorities sum part 2: %d", part2PrioritiesSum(input));
     }
 
-    private final static Map<String, Integer> weights = new HashMap<>() {{
-        put("a", 1);
-        put("b", 2);
-        put("c", 3);
-        put("d", 4);
-        put("e", 5);
-        put("f", 6);
-        put("g", 7);
-        put("h", 8);
-        put("i", 9);
-        put("j", 10);
-        put("k", 11);
-        put("l", 12);
-        put("m", 13);
-        put("n", 14);
-        put("o", 15);
-        put("p", 16);
-        put("q", 17);
-        put("r", 18);
-        put("s", 19);
-        put("t", 20);
-        put("u", 21);
-        put("v", 22);
-        put("w", 23);
-        put("x", 24);
-        put("y", 25);
-        put("z", 26);
-        put("A", 27);
-        put("B", 28);
-        put("C", 29);
-        put("D", 30);
-        put("E", 31);
-        put("F", 32);
-        put("G", 33);
-        put("H", 34);
-        put("I", 35);
-        put("J", 36);
-        put("K", 37);
-        put("L", 38);
-        put("M", 39);
-        put("N", 40);
-        put("O", 41);
-        put("P", 42);
-        put("Q", 43);
-        put("R", 44);
-        put("S", 45);
-        put("T", 46);
-        put("U", 47);
-        put("V", 48);
-        put("W", 49);
-        put("X", 50);
-        put("Y", 51);
-        put("Z", 52);
-    }};
+    private final static Map<String, Integer> weights;
+
+    static {
+        record Pair(char c, int index) {
+        }
+
+        AtomicInteger count = new AtomicInteger(1);
+        weights = IntStream.concat(IntStream.range(97, 123), IntStream.range(65, 91))
+                .mapToObj(i -> new Pair((char) i, count.getAndIncrement()))
+                .collect(toMap(p -> String.valueOf(p.c), p -> p.index));
+    }
 
     static int part1PrioritiesSum(List<String> input) {
         return input.stream()

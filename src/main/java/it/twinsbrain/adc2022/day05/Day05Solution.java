40de66f37ed4029cb20cf8a051b7e3f460a8e1f8
Paolo Sciarra <itbhps@gmail.com>
2022-12-05 19:51:21 +0100

    [adc2022] day05 [WIP]

---
+++ src/main/java/it/twinsbrain/adc2022/day05/Day05Solution.java
@@ -0,0 +1,77 @@
+package it.twinsbrain.adc2022.day05;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day05Solution {
+
+    public static void main(String[] args) {
+        Deque<Integer> l = new LinkedList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+
+        System.out.println(l.peek());
+
+        l.removeFirst();
+
+        System.out.println(l.peek());
+
+        l.addFirst(4);
+
+        System.out.println(l.peek());
+    }
+
+    public static ArrayList<LinkedList<String>> parseCrates(List<String> input) {
+        var cratesColumns = getCratesColumns(input);
+        var crates = newArrayList(cratesColumns);
+        var regexp = Pattern.compile("\\[([A-Z])]\\s");
+        input.stream().takeWhile(s -> !s.matches("\\.*\\d+\\.*"))
+                .forEach(line -> {
+                    List<String> columnsItems = chunked(line, 4).toList();
+                    for (int i = 0; i < columnsItems.size(); i++) {
+                        var columnItem = columnsItems.get(i);
+                        if (columnItem.trim().length() > 0) {
+                            Matcher matcher = regexp.matcher(columnItem);
+                            if (matcher.find()) {
+                                var id = matcher.group(1);
+                                crates.get(i).add(id);
+                            }
+                        }
+                    }
+                });
+        return crates;
+    }
+
+    private static int getCratesColumns(List<String> input) {
+        int length = input.get(0).length();
+        int div = length / 4;
+        return div * 4 == length ? div : div + 1;
+    }
+
+    private static ArrayList<LinkedList<String>> newArrayList(int cratesColumns) {
+        ArrayList<LinkedList<String>> linkedLists = new ArrayList<>(cratesColumns);
+        for (int i = 0; i < cratesColumns; i++) {
+            linkedLists.add(new LinkedList<>());
+        }
+        return linkedLists;
+    }
+
+    public static Stream<String> chunked(String input, int chunkSize) {
+        AtomicInteger counter = new AtomicInteger();
+        return input
+                .chars()
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / chunkSize))
+                .values()
+                .stream()
+                .map(l -> l.stream().map(String::valueOf).collect(Collectors.joining()));
+    }
+}
---
+++ src/test/java/it/twinsbrain/adc2022/day05/Day05SolutionTest.java
@@ -0,0 +1,115 @@
+package it.twinsbrain.adc2022.day05;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class Day05SolutionTest {
+
+    @Test
+    void parseCratesStacks() {
+        List<String> input = List.of(
+                "        [H]     [W] [B]           ",
+                "    [D] [B]     [L] [G] [N]       ",
+                "[P] [J] [T]     [M] [R] [D]       ",
+                "[V] [F] [V]     [F] [Z] [B]     [C]",
+                "[Z] [V] [S]     [G] [H] [C] [Q] [R]",
+                "[W] [W] [L] [J] [B] [V] [P] [B] [Z]",
+                "[D] [S] [M] [S] [Z] [W] [J] [T] [G]",
+                "[T] [L] [Z] [R] [C] [Q] [V] [P] [H]",
+                " 1   2   3   4   5   6   7   8   9"
+        );
+
+        var expected = new ArrayList<LinkedList<String>>(9);
+        expected.add(new LinkedList<>() {{
+            add("P");
+            add("V");
+            add("Z");
+            add("W");
+            add("D");
+            add("T");
+        }});
+
+        expected.add(new LinkedList<>() {{
+            add("D");
+            add("J");
+            add("F");
+            add("V");
+            add("W");
+            add("S");
+            add("L");
+        }});
+
+        expected.add(new LinkedList<>() {{
+            add("H");
+            add("B");
+            add("T");
+            add("V");
+            add("S");
+            add("L");
+            add("M");
+            add("Z");
+        }});
+
+        expected.add(new LinkedList<>() {{
+            add("J");
+            add("S");
+            add("R");
+            add("Z");
+        }});
+
+        expected.add(new LinkedList<>() {{
+            add("W");
+            add("L");
+            add("M");
+            add("F");
+            add("G");
+            add("B");
+            add("Z");
+            add("C");
+        }});
+
+        expected.add(new LinkedList<>() {{
+            add("B");
+            add("G");
+            add("R");
+            add("Z");
+            add("H");
+            add("V");
+            add("W");
+            add("Q");
+        }});
+
+        expected.add(new LinkedList<>() {{
+            add("N");
+            add("D");
+            add("B");
+            add("C");
+            add("P");
+            add("J");
+            add("V");
+        }});
+
+        expected.add(new LinkedList<>() {{
+            add("Q");
+            add("B");
+            add("T");
+            add("P");
+        }});
+
+        expected.add(new LinkedList<>() {{
+            add("C");
+            add("R");
+            add("Z");
+            add("G");
+            add("H");
+        }});
+
+        assertThat(Day05Solution.parseCrates(input), equalTo(expected));
+    }
+}

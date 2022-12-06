2ff902841188b33bdb1a3b147fc7d7826ac8435a
Paolo Sciarra <itbhps@gmail.com>
2022-12-06 15:57:26 +0100

    [adc2022] day06 solved

--- src/main/java/it/twinsbrain/adc2022/day06/Day06Solution.java
+++ src/main/java/it/twinsbrain/adc2022/day06/Day06Solution.java
@@ -1,14 +1,33 @@
 package it.twinsbrain.adc2022.day06;
 
+import java.net.URISyntaxException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static it.twinsbrain.adc2022.FilesModule.read;
+import static it.twinsbrain.adc2022.FilesModule.resource;
+
 public class Day06Solution {
 
+    public static void main(String[] args) throws URISyntaxException {
+        var input = read(resource("/day06/input.txt"));
+        System.out.printf("Part 1: %s", endPositionFirstMarker(input));
+        System.out.println();
+        System.out.printf("Part 2: %s", endPositionFirstMessageMarker(input));
+    }
+
+    public static int endPositionFirstMessageMarker(List<String> input) {
+        return findMarker(input, 14);
+    }
+
     public static int endPositionFirstMarker(List<String> input) {
+        return findMarker(input, 4);
+    }
+
+    private static int findMarker(List<String> input, int size) {
         String buffer = input.get(0);
-        var window = new Window(4);
+        var window = new Window(size);
         AtomicInteger index = new AtomicInteger(1);
         buffer.chars()
                 .mapToObj(i -> (char) i)
--- src/test/java/it/twinsbrain/adc2022/day06/Day06SolutionTest.java
+++ src/test/java/it/twinsbrain/adc2022/day06/Day06SolutionTest.java
@@ -1,11 +1,17 @@
 package it.twinsbrain.adc2022.day06;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
+import static it.twinsbrain.adc2022.FilesModule.read;
+import static it.twinsbrain.adc2022.FilesModule.resource;
 import static it.twinsbrain.adc2022.day06.Day06Solution.endPositionFirstMarker;
+import static it.twinsbrain.adc2022.day06.Day06Solution.endPositionFirstMessageMarker;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -16,7 +22,39 @@ class Day06SolutionTest {
             "grvrmped, 6",
             "grvtsert, 4",
     })
-    void itShouldWork(String input, int expectedPosition) {
+    void part1ShouldWork(String input, int expectedPosition) {
         assertThat(endPositionFirstMarker(List.of(input)), equalTo(expectedPosition));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "mjqjpqmgbljsphdztnvjfqwrcgsmlb, 19",
+            "bvwbjplbgvbhsrlpgdmjqwftvncz, 23",
+    })
+    void part2ShouldWork(String input, int expectedPosition) {
+        assertThat(endPositionFirstMessageMarker(List.of(input)), equalTo(expectedPosition));
+    }
+
+    @Nested
+    class AcceptanceTest {
+        private static final List<String> input;
+
+        static {
+            try {
+                input = read(resource("/day06/input.txt"));
+            } catch (URISyntaxException e) {
+                throw new RuntimeException("input file not found!");
+            }
+        }
+
+        @Test
+        void part1Test() {
+            assertThat(endPositionFirstMarker(input), equalTo(1480));
+        }
+
+        @Test
+        void part2Test() {
+            assertThat(endPositionFirstMessageMarker(input), equalTo(2746));
+        }
     }
 }

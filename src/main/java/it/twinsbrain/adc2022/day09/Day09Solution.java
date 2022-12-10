ff3b5864573bbbdb6389568ccad1b339222ee325
Paolo Sciarra <itbhps@gmail.com>
2022-12-10 12:28:47 +0100

    [adc2022] day09 [wip] not working

--- src/main/java/it/twinsbrain/adc2022/day09/Day09Solution.java
+++ src/main/java/it/twinsbrain/adc2022/day09/Day09Solution.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.abs;
 
 public class Day09Solution {
 
@@ -47,6 +50,21 @@ public class Day09Solution {
 
 
     record Point(int x, int y) {
+        boolean sameColumn(Point other) {
+            return this.x == other.x;
+        }
+
+        boolean sameRow(Point other) {
+            return this.y == other.y;
+        }
+
+        int xDistance(Point other) {
+            return other.x - this.x;
+        }
+
+        int yDistance(Point other) {
+            return other.y - this.y;
+        }
     }
 
     /**
@@ -65,17 +83,75 @@ public class Day09Solution {
         private Point tail = new Point(0, 0);
         private final Set<Point> tailTrail = new HashSet<>();
 
+        public Journey() {
+            tailTrail.add(tail);
+        }
+
         public void accept(Command command) {
             switch (command) {
-                case Left steps -> head = new Point(head.x - steps.amount, head.y);
-                case Right steps -> head = new Point(head.x + steps.amount, head.y);
-                case Up steps -> head = new Point(head.x, head.y - steps.amount);
-                case Down steps -> head = new Point(head.x, head.y + steps.amount);
+                case Left steps -> IntStream.range(1, steps.amount + 1).forEach(step -> {
+                    head = new Point(head.x - step, head.y);
+                    updateTail();
+                });
+                case Right steps -> IntStream.range(1, steps.amount + 1).forEach(step -> {
+                    head = new Point(head.x + 1, head.y);
+                    updateTail();
+                });
+                case Up steps -> IntStream.range(1, steps.amount + 1).forEach(step -> {
+                    head = new Point(head.x, head.y - 1);
+                    updateTail();
+                });
+                case Down steps -> IntStream.range(1, steps.amount + 1).forEach(step -> {
+                    head = new Point(head.x, head.y + 1);
+                    updateTail();
+                });
             }
         }
 
+        private void updateTail() {
+
+            if (tail.sameRow(head) && tail.xDistance(head) > 1) {
+                var newTail = new Point(tail.x + 1, tail.y);
+                tail = newTail;
+                addTrail(newTail);
+            } else if (tail.sameColumn(head) && tail.yDistance(head) > 1) {
+                var newTail = new Point(tail.x, tail.y + 1);
+                tail = newTail;
+                addTrail(newTail);
+            } else if (tail.sameRow(head) && tail.xDistance(head) < -1) {
+                var newTail = new Point(tail.x - 1, tail.y);
+                tail = newTail;
+                addTrail(newTail);
+            } else if (tail.sameColumn(head) && tail.yDistance(head) < -1) {
+                var newTail = new Point(tail.x, tail.y - 1);
+                tail = newTail;
+                addTrail(newTail);
+            } else if (!tail.sameColumn(head) && !tail.sameRow(head)) {
+                var xDistance = tail.xDistance(head);
+                var yDistance = tail.yDistance(head);
+                if (abs(xDistance) != 1 || abs(yDistance) != 1) {
+                    var deltaX = computeDelta(xDistance);
+                    var deltaY = computeDelta(yDistance);
+                    var newTail = new Point(tail.x + deltaX, tail.y + deltaY);
+                    tail = newTail;
+                    addTrail(newTail);
+                }
+            }
+        }
+
+        private int computeDelta(int distance) {
+            if (distance < 0) {
+                return -1;
+            }
+            return 1;
+        }
+
+        private void addTrail(Point newTail) {
+            tailTrail.add(newTail);
+        }
+
         public int howManyTailUniquePositions() {
-            return 0;
+            return tailTrail.size();
         }
     }
 

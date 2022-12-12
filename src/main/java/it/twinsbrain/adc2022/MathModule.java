c9f3f7a1595129321dd309ba6a513f64408ae529
Paolo Sciarra <itbhps@gmail.com>
2022-12-12 13:59:21 +0100

    [day11] solved part2 ... (use always higher level functions :D)

--- src/main/java/it/twinsbrain/adc2022/MathModule.java
+++ src/main/java/it/twinsbrain/adc2022/MathModule.java
@@ -16,16 +16,6 @@ public class MathModule {
     }
 
     public static long lcm(List<Long> elements) {
-        if (elements.size() <= 1) {
-            return elements.stream().findFirst().orElse(1L);
-        }
-        long prev = elements.get(0);
-        long res = prev;
-        for (int i = 1; i < elements.size(); i++) {
-            var elem = elements.get(i);
-            res = lcm(prev, elem);
-            prev = elem;
-        }
-        return res;
+        return elements.stream().reduce(1L, MathModule::lcm);
     }
 }
---
+++ src/test/java/it/twinsbrain/adc2022/MathModuleTest.java
@@ -0,0 +1,30 @@
+package it.twinsbrain.adc2022;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class MathModuleTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "45, 20, 5",
+            "60, 90, 30",
+            "42, 210, 42"
+    })
+    void gcd_should_work(long a, long b, long expected) {
+        assertThat(MathModule.gcd(a, b), is(expected));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "45, 20, 180", // 45 = 3^2 * 5, 20 = 2^2 * 5 => lcm = 2^2 * 3^2 * 5 = 180
+            "60, 90, 180", // 50 = 2^2 * 3 * 5, 90 = 2 * 3^2 * 5 => lcm = 2^2 * 3^2 * 5 = 180
+            "42, 210, 210"  // 42 = 2 * 3 * 7, 210 = 2 * 3 * 5 * 7 => lcm = 2 * 3 * 5 * 7 = 210
+    })
+    void lcm_should_work(long a, long b, long expected) {
+        assertThat(MathModule.lcm(a, b), is(expected));
+    }
+}

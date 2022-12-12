f694be61334c30959f3d4f48a78bf88d5da492c8
Paolo Sciarra <itbhps@gmail.com>
2022-12-12 21:30:46 +0100

    [day12] refactored parsing

--- src/main/java/it/twinsbrain/adc2022/day12/Day12Solution.java
+++ src/main/java/it/twinsbrain/adc2022/day12/Day12Solution.java
@@ -1,21 +1,93 @@
 package it.twinsbrain.adc2022.day12;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Day12Solution {
 
-    public static char[][] parse(List<String> input) {
-        var rows = input.size();
-        char[][] res = new char[rows][];
-        for (int j = 0; j < input.size(); j++) {
-            String line = input.get(j);
+    public static int part1(List<String> input) {
+//        Grid grid = parse(input);
+        throw new UnsupportedOperationException();
+    }
+
+    static class Grid {
+        public char[][] grid;
+        private int startX = -1;
+        private int startY = -1;
+
+        private int endX = -1;
+        private int endY = -1;
+
+        public Grid(int numberOfRows) {
+            grid = new char[numberOfRows][];
+        }
+
+        public Grid(char[][] grid, int startX, int startY, int endX, int endY) {
+            this.grid = grid;
+            this.startX = startX;
+            this.startY = startY;
+            this.endX = endX;
+            this.endY = endY;
+        }
+
+        public void acceptRow(int rowNumber, String line) {
             char[] row = new char[line.length()];
             String[] split = line.split("");
             for (int i = 0; i < split.length; i++) {
-                row[i] = split[i].toCharArray()[0];
+                String s = split[i];
+                if (s.equals("S")) {
+                    startX = rowNumber;
+                    startY = i;
+                }
+                if (s.equals("E")) {
+                    endX = rowNumber;
+                    endY = i;
+                }
+                row[i] = s.toCharArray()[0];
             }
-            res[j] = row;
+            grid[rowNumber] = row;
         }
-        return res;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Grid grid1 = (Grid) o;
+            return startX == grid1.startX
+                    && startY == grid1.startY
+                    && endX == grid1.endX
+                    && endY == grid1.endY
+                    && Arrays.deepEquals(grid, grid1.grid);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(startX, startY, endX, endY);
+            result = 31 * result + Arrays.deepHashCode(grid);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Grid{" +
+                    "grid=" + Arrays.stream(grid).map(Arrays::toString).collect(Collectors.joining("")) +
+                    ", startX=" + startX +
+                    ", startY=" + startY +
+                    ", endX=" + endX +
+                    ", endY=" + endY +
+                    '}';
+        }
+    }
+
+    public static Grid parse(List<String> input) {
+        var rows = input.size();
+        var grid = new Grid(rows);
+        for (int j = 0; j < input.size(); j++) {
+            String line = input.get(j);
+            grid.acceptRow(j, line);
+        }
+        return grid;
     }
 }
--- src/test/java/it/twinsbrain/adc2022/day12/Day12SolutionTest.java
+++ src/test/java/it/twinsbrain/adc2022/day12/Day12SolutionTest.java
@@ -1,9 +1,13 @@
 package it.twinsbrain.adc2022.day12;
 
+import it.twinsbrain.adc2022.FixtureModule;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static it.twinsbrain.adc2022.day12.Day12Solution.parse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -15,10 +19,23 @@ class Day12SolutionTest {
                 "Sabc",
                 "bssE"
         );
-        var expected = new char[2][4];
-        expected[0] = new char[]{'S', 'a', 'b', 'c'};
-        expected[1] = new char[]{'b', 's', 's', 'E'};
+        var chars = new char[2][4];
+        chars[0] = new char[]{'S', 'a', 'b', 'c'};
+        chars[1] = new char[]{'b', 's', 's', 'E'};
+        var expectedGrid = new Day12Solution.Grid(chars, 0, 0, 1, 3);
 
-        assertThat(Day12Solution.parse(input), equalTo(expected));
+        assertThat(parse(input), equalTo(expectedGrid));
+    }
+
+    @Nested
+    class AcceptanceTest {
+
+        private final static List<String> input = FixtureModule.readSample("day12");
+
+        @Test
+        @Disabled
+        void part1_should_work() {
+            assertThat(Day12Solution.part1(input), equalTo(31));
+        }
     }
 }
